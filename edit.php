dc00e3e259b47d7e0776ff04c3d19af4c6b1224b
abizeleke <132948928+abizeleke@users.noreply.github.com>
2023-11-28 08:38:33 +0300

    Update edit.php

--- edit.php
+++ edit.php
@@ -31,48 +31,4 @@
     
     
   ?> 
-  <!--
-    $query2 = "SELECT `no`, `fullName`, `id`, `department`, `year`, `phoneNo`, `email`, `password`, `secQstn`, `secAns` FROM `users` WHERE id = '".$email."'" ;
-    $result2= mysqli_query($con ,$query2);
-    $row2=mysqli_fetch_assoc($result2);
-    $email1 = $row2['email'];
-    if($id1===$id){
-        header("location:register.php?inf=invalidId");
-    }
-    else if($email1===$email){
-        header("location:register.php?inf=invalidEmail");
-    }
-
-
-    $query = "SELECT width FROM modules WHERE code = '".$module."'";
-
-
-
-    if($row1=mysqli_fetch_assoc($result1)){
-    $id2 = $row1['id'];
-    if($id2 === $id){
-        header("location:register.php?inf=invalidId");
-    }
-}
-else if($row2=mysqli_fetch_assoc($result2)){
-    $email2 = $row2['email'];
-    if($email2 === $email){
-        header("location:register.php?inf2=invalidEmail");
-    }
-}
-else{
-        
-    $qq="INSERT INTO `unapproved`(`fullName`, `id`, `department`, `year`, `phoneNo`, `email`, `password`, `secQstn`, `secAns`) VALUES ('$fullName','$id','$department','$year','$phoneNo','$email','$userPassword','$secQstn','$secAns')";
-
-    $result = mysqli_query($con, $qq);
-    if($result){
-        header("location:index.php?msg1=registerd");
-    }
-    else{
-
-        header("location:register.php");
-    }  
-}
-
-    
-  -->
+  
